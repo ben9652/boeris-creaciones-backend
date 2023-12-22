09b208d23b56e8c679b502b77d6f8e9368bae1ea
Benjamín Boeri <ben9652@gmail.com>
2023-12-22 19:23:36 -0300

    Se agregan los repositories y services de Usuarios

---
+++ BoerisCreaciones.Repository/Repositories/UsuariosRepository.cs
@@ -0,0 +1,23 @@
+﻿using BoerisCreaciones.Core;
+using BoerisCreaciones.Core.Models;
+using BoerisCreaciones.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoerisCreaciones.Repository.Repositories
+{
+    public class UsuariosRepository : Repository<Usuario>, IUsuariosRepository
+    {
+        private readonly ApplicationConfig _applicationConfig;
+        private readonly BoerisCreacionesContext ctx;
+
+        public UsuariosRepository(ApplicationConfig applicationConfig, BoerisCreacionesContext context) : base(context)
+        {
+            _applicationConfig = applicationConfig;
+            ctx = context;
+        }
+    }
+}
---
+++ BoerisCreaciones.Service/Services/UsuariosService.cs
@@ -0,0 +1,20 @@
+﻿using BoerisCreaciones.Repository.Interfaces;
+using BoerisCreaciones.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoerisCreaciones.Service.Services
+{
+    public class UsuariosService : IUsuariosService
+    {
+        private readonly IUsuariosRepository _repository;
+
+        public UsuariosService(IUsuariosRepository usuariosRepository)
+        {
+            _repository = usuariosRepository;
+        }
+    }
+}
