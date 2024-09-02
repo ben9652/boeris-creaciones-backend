97670271d30584eebbc648ad85af833b21eb67cc
Benjamín Boeri <ben9652@gmail.com>
2024-09-02 11:43:05 -0300

    Creo el repositorio de Proveedores

--- BoerisCreaciones.Repository/Repositories/CatalogoMateriasPrimasRepository.cs
+++ BoerisCreaciones.Repository/Repositories/CatalogoMateriasPrimasRepository.cs
@@ -2,12 +2,7 @@
 using BoerisCreaciones.Core.Models.MateriasPrimas;
 using BoerisCreaciones.Repository.Interfaces;
 using MySql.Data.MySqlClient;
-using System;
-using System.Collections.Generic;
 using System.Data.Common;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BoerisCreaciones.Repository.Repositories
 {
