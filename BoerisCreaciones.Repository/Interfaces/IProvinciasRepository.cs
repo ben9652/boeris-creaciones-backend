8436b72ac869bc6d97f219a7fd930569710545a2
Benjamín Boeri <ben9652@gmail.com>
2024-09-03 11:33:00 -0300

    Creo el controlador para la gestión de las provincias

---
+++ BoerisCreaciones.Repository/Interfaces/IProvinciasRepository.cs
@@ -0,0 +1,13 @@
+﻿using BoerisCreaciones.Core.Models;
+
+namespace BoerisCreaciones.Repository.Interfaces
+{
+    public interface IProvinciasRepository
+    {
+        public List<ProvinciaVM> GetAllProvinces();
+        public ProvinciaVM GetProvince(int id);
+        public ProvinciaVM CreateProvince(ProvinciaVM provincia);
+        public ProvinciaVM UpdateProvince(ProvinciaVM provincia, List<string> attributesToChange);
+        public void DeleteProvince(int id);
+    }
+}
---
+++ BoerisCreaciones.Service/Interfaces/IProvinciasService.cs
@@ -0,0 +1,13 @@
+﻿using BoerisCreaciones.Core.Models;
+
+namespace BoerisCreaciones.Service.Interfaces
+{
+    public interface IProvinciasService
+    {
+        public List<ProvinciaDTO> GetAllProvinces();
+        public ProvinciaDTO GetProvince(int id);
+        public ProvinciaDTO CreateProvince(ProvinciaDTO provincia);
+        public ProvinciaDTO UpdateProvince(ProvinciaDTO provincia, List<string> attributes);
+        public void DeleteProvince(int id);
+    }
+}
